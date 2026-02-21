51bca9749dc485c046c31c444370d535f493f482
Ahmed Salama <Ahmed.Salama@integrant.com>
2026-02-21 17:09:50 +0200

    feat: store uploaded images in AWS S3

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -30,7 +30,11 @@ export default function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image
+            src={`https://maxschwarzmueller-nextjs-demo-users-image.s3.amazonaws.com/${meal.image}`}
+            alt={meal.title}
+            fill
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
